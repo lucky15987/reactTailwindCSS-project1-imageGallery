915d24fb8dc0f727803b6259f4b6da9a972f48fb
Ethan N <lucky35789@yahoo.com>
2022-08-01 13:37:27 -0400

    added scripts for tailwinds.css and output main.css

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <p class="bg-white text-lg">Testing for Tailwind</p>
         
       </header>
     </div>
